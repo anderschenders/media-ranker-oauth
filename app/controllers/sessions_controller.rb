b0357d33624bb33786bf6a5b0d9261dd17239700
Andrea Chen <andreachen.ac@gmail.com>
2017-10-16 12:25:31 -0700

    Add content to SessionsController#create. Add content to ApplicationsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,41 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:login]
 
   def create
     auth_hash = request.env['omniauth.auth']
-    raise
+
+    if auth_hash['uid']
+      user = User.find_by(provider: params[:provider], uid: auth_hash['uid'])
+      if user.nil?
+        # User has not logged in before
+        # Create a new record in the DB
+        user = User.from_auth_hash(params[:provider], auth_hash)
+        save_and_flash(user)####
+
+      else
+        session[:user_id] = user.id
+        flash[:status] = :success
+        flash[:message] = "Successfully logged in as returning user #{user.name}"
+
+      end
+
+    else
+      flash[:status] = :failure
+      flash[:message] = "Could not create user from OAuth data"
+    end
+
+    redirect_to root_path
   end
 
-  def login_form
+  def logout
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:message] = "You have been logged out"
+    redirect_to root_path
   end
+
+  # def login_form
+  # end
 
   # def login
   #   username = params[:username]
@@ -30,11 +59,11 @@ class SessionsController < ApplicationController
   #   end
   #   redirect_to root_path
   # end
-
-  def logout
-    session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
-    redirect_to root_path
-  end
+  #
+  # def logout
+  #   session[:user_id] = nil
+  #   flash[:status] = :success
+  #   flash[:result_text] = "Successfully logged out"
+  #   redirect_to root_path
+  # end
 end
